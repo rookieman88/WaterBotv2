a45eabb3568f8ec806aa6f416656a807e906d348
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-03 21:23:05 +0900

    Update 도움.js

--- commands/도움.js
+++ commands/도움.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
       const sayMessage = args.join(" ");
-      message.delete().catch();
       message.channel.send(`사실상 테스트 [ ${sayMessage} ]`);
 
 }
