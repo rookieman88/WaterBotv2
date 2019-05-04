deb09b96ce03578cd357865ba65e12d69fa0a2ba
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-04 19:59:15 +0900

    Update 차트.js

--- commands/차트.js
+++ commands/차트.js
@@ -25,34 +25,34 @@ let CP4 = ForP - ForPs
 let CP5 = FivP - FivPs
 
 if (OneP > OnePs) {
-     CP1 = `+ 회사1 : (` + ` + ${CP1})`
+     CP1 = `+ 회사1 : ${OneP} (` + ` + ${CP1})`
 } else if (OneP < OnePs) {
-     CP1 = `- 회사1 : (` + ` ${CP1})`
-} else { CP1 = `0 회사1 : - 0` }
+     CP1 = `- 회사1 : ${OneP} (` + ` ${CP1})`
+} else { CP1 = `0 회사1 : ${OneP} ( - 0 )` }
                 
 if (TwoP > TwoPs) {
-     CP2 = `+ 회사2 : (` + ` + ${CP2})`
+     CP2 = `+ 회사2 : ${TwoP} (` + ` + ${CP2})`
 } else if (TwoP < TwoPs) {
-     CP2 = `- 회사2 : (` + ` ${CP2})`
-} else { CP2 = `0 회사1 : - 0` }
+     CP2 = `- 회사2 : ${TwoP} (` + ` ${CP2})`
+} else { CP2 = `0 회사1 : ${TwoP} ( - 0 )` }
                 
 if (ThrP > ThrPs) {
-     CP3 = `+ 회사3 : (` + ` + ${CP3})`
+     CP3 = `+ 회사3 : ${ThrP} (` + ` + ${CP3})`
 } else if (ThrP < ThrPs) {
-     CP3 = `- 회사3 : (` + ` ${CP3})`
-} else { CP3 = `0 회사3 : - 0` }
+     CP3 = `- 회사3 : ${ThrP} (` + ` ${CP3})`
+} else { CP3 = `0 회사3 : ${ThrP} ( - 0 )` }
                 
 if (ForP > ForPs) {
-     CP4 = `+ 회사4 : (` + ` + ${CP4})`
+     CP4 = `+ 회사4 : ${ForP} (` + ` + ${CP4})`
 } else if (ForP < ForPs) {
-     CP4 = `- 회사4 : (` + ` ${CP4})`
-} else { CP4 = `0 회사4 : - 0` }
+     CP4 = `- 회사4 : ${ForP} (` + ` ${CP4})`
+} else { CP4 = `0 회사4 : ${ForP} ( - 0 )` }
                 
 if (FivP > FivPs) {
-     CP5 = `+ 회사5 : (` + ` + ${CP5})`
+     CP5 = `+ 회사5 : ${FivP} (` + ` + ${CP5})`
 } else if (FivP < FivPs) {
-     CP5 = `- 회사5 : (` + ` ${CP5})`
-} else { CP5 = `0 회사5 : - 0` }
+     CP5 = `- 회사5 : ${FivP} (` + ` ${CP5})`
+} else { CP5 = `0 회사5 : ${FivP} ( - 0 )` }
      
  
 
