9767fe07387fb19833814e2bfe74830cea90afbd
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-04 18:13:27 +0900

    Update 차트.js

--- commands/차트.js
+++ commands/차트.js
@@ -15,13 +15,13 @@ let FivP = StocksPrice["Stocks"].FivStock
 
 message.channel.send("< 시즌 베타 1 주식 차트>\n(차트는 1분에 한번씩 업데이트됩니다.)" + `
 
-`\`\`
+\`\`\`
 회사1 : ${OneP}원
 회사2 : ${TwoP}원
 회사3 : ${ThrP}원
 회사4 : ${ForP}원
 회사5 : ${FivP}원
-`\`\`
+\`\`\`
 
  ` +
 "버그를 발견하신 분은 ~문의 로 버그제보좀"
