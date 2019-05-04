ff2205a2037bbd5d384758be067f78b581147691
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-04 19:54:17 +0900

    Update 차트.js

--- commands/차트.js
+++ commands/차트.js
@@ -3,7 +3,7 @@ const superagent = require('superagent');
 
 module.exports.run = async (bot, message, args) => {
 
-     	 superagent.get("https://api.myjson.com/bins/15in2s").then((res) => {
+     	 superagent.get("https://api.myjson.com/bins/152s38").then((res) => {
 let StocksPrice = res.body;
 
 let OneP = StocksPrice["Stocks"].OneStock
@@ -11,16 +11,59 @@ let TwoP = StocksPrice["Stocks"].TwoStock
 let ThrP = StocksPrice["Stocks"].ThrStock
 let ForP = StocksPrice["Stocks"].ForStock
 let FivP = StocksPrice["Stocks"].FivStock
+
+let OnePs = StocksPrice["StocksB"].OneStockB
+let TwoPs = StocksPrice["StocksB"].TwoStockB
+let ThrPs = StocksPrice["StocksB"].ThrStockB
+let ForPs = StocksPrice["StocksB"].ForStockB
+let FivPs = StocksPrice["StocksB"].FivStockB
+
+let CP1 = OneP - OnePs
+let CP2 = TwoP - TwoPs
+let CP3 = ThrP - ThrPs
+let CP4 = ForP - ForPs
+let CP5 = FivP - FivPs
+
+if (OneP > OnePs) {
+     CP1 = `+ 회사1 : (` + ` + ${CP1})`
+} else if (OneP < OnePs) {
+     CP1 = `- 회사1 : (` + ` ${CP1})`
+} else { CP1 = `0 회사1 : - 0` }
+                
+if (TwoP > TwoPs) {
+     CP2 = `+ 회사2 : (` + ` + ${CP2})`
+} else if (TwoP < TwoPs) {
+     CP2 = `- 회사2 : (` + ` ${CP2})`
+} else { CP2 = `0 회사1 : - 0` }
+                
+if (ThrP > ThrPs) {
+     CP3 = `+ 회사3 : (` + ` + ${CP3})`
+} else if (ThrP < ThrPs) {
+     CP3 = `- 회사3 : (` + ` ${CP3})`
+} else { CP3 = `0 회사3 : - 0` }
+                
+if (ForP > ForPs) {
+     CP4 = `+ 회사4 : (` + ` + ${CP4})`
+} else if (ForP < ForPs) {
+     CP4 = `- 회사4 : (` + ` ${CP4})`
+} else { CP1 = `0 회사4 : - 0` }
+                
+if (FivP > FivPs) {
+     CP5 = `+ 회사5 : (` + ` + ${CP5})`
+} else if (FivP < FivPs) {
+     CP5 = `- 회사5 : (` + ` ${CP5})`
+} else { CP5 = `0 회사5 : - 0` }
+     
  
 
 message.channel.send("< 시즌 베타 1 주식 차트>\n(차트는 1분에 한번씩 업데이트됩니다.)" + `
 
-\`\`\`
-회사1 : ${OneP}원
-회사2 : ${TwoP}원
-회사3 : ${ThrP}원
-회사4 : ${ForP}원
-회사5 : ${FivP}원
+\`\`\`diff
+${CP1}
+${CP2}
+${CP3}
+${CP4}
+${CP5}
 \`\`\`
 
  ` +
