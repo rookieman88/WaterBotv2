9de6b513882274248b84013f3e6946e607bc20f8
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-05 10:43:46 +0900

    Update 골라.js

--- commands/골라.js
+++ commands/골라.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+	if (!args[1]) { return message.channel.send('2개 이상 입력해야 선택이 가능합니다!') }
     let say = args.join(" ").slice(0);
     let choses = say.split(" ");
     let ranChose = Math.floor(Math.random() * (choses.length) + 0);
