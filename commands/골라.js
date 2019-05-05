baca5db3dc083b059bcf31c56f3aeace4a39d20e
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-05 10:42:07 +0900

    Update and rename choose.js to 골라.js

--- commands/골라.js
+++ commands/골라.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     let say = args.join(" ").slice(0);
     let choses = say.split(" ");
     let ranChose = Math.floor(Math.random() * (choses.length) + 0);
-    message.channel.send(`"${choses[ranChose]}"~`)
+    message.channel.send(`"${choses[ranChose]}"`)
 };
 
 module.exports.help = {
