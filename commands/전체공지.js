40e8b140cdbbbf95f49b18c19eda09f052b0f721
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-12 19:43:01 +0900

    Update 전체공지.js

--- commands/전체공지.js
+++ commands/전체공지.js
@@ -6,7 +6,7 @@ const superagent = require('superagent');
  
 		
 
-
+let filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '⭕') && user.id === message.author.id
 
 		
  let owner = "417571990820618250"
@@ -17,6 +17,17 @@ const superagent = require('superagent');
  if (owners.includes(message.author.id)) {
 	 
 	 if(args[0] === '에브리원') {
+		 
+ let reason1 = message.content.replace(`워터야 공지 에브리원 `, "")
+ message.channel.send(`${bot.guilds.size}개의 서버에 공지가 발신됩니다. 공지 내용은 다음과 같습니다\n${reason1}`).then((th) => {
+      th.react('❌')
+      th.react('⭕')
+		 
+th.awaitReactions(filter, {
+        time: 30000,
+        max: 1
+}).then((collected) => {
+ if (collected.array()[0].emoji.name === '⭕') {
 
 	 superagent.get("https://api.myjson.com/bins/sztu0").then((res) => {
 let welcomechannel = res.body;
@@ -47,9 +58,20 @@ let Ch = bot.channels.get(cha)
 공지 내용은 [ ${reason} + everyone ] 입니다.
 `)
 	 });
-		 
+	  } else { message.channel.send('공지 발신이 취소되었습니다.') }
+	 });
+ });
 	 } else {
+		  let reason1 = message.content.replace(`워터야 공지 에브리원 `, "")
+ message.channel.send(`${bot.guilds.size}개의 서버에 공지가 발신됩니다. 공지 내용은 다음과 같습니다\n${reason1}`).then((th) => {
+      th.react('❌')
+      th.react('⭕')
 		 
+th.awaitReactions(filter, {
+        time: 30000,
+        max: 1
+}).then((collected) => {
+ if (collected.array()[0].emoji.name === '⭕') {
 	
 	 superagent.get("https://api.myjson.com/bins/sztu0").then((res) => {
 let welcomechannel = res.body;
@@ -78,7 +100,9 @@ let Ch = bot.channels.get(cha)
 발신이 완료되었습니다!
 공지 내용은 [ ${reason} ] 입니다.
 `)
-	 });	 
+	 });
+ } else { message.channel.send('공지 발신이 취소되었습니다.') }
+ });
 		 
 	 }
  } else {
