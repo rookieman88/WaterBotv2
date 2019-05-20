fd7d3416a6d71afe8b44ec405803027b2a1eb90b
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-05-20 21:09:24 +0900

    Add files via upload

--- commands/끝말잇기.js
+++ commands/끝말잇기.js
@@ -1,57 +1,128 @@
+
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-/*
-message.channel.send('끝말잇기를 시작합니다. 1분안에 2명이 모이지 않으면 종료됩니다.').then((msg) => {
- msg.react('🎉')
-const filter = (reaction, user) => reaction.emoji.name === '🎉' && user.id !== bot.user.id
-      msg.awaitReactions(filter, {
-        maxUsers: 2
-}).then((collected) => {
-        let p1 = collected.array()[0].users.array()[1].id
-        let p2 = collected.array()[0].users.array()[2].id
+        const readline = require('readline');
+        const fs = require('fs');
+        let dict = fs.readFileSync('./commands/dict.txt').toString().replace(/\r/g,"").split('\n')
+        let p1 = 0
+        let p2 = 0
         
-        let thip = 0
-        let endgame = 0
-        let eln
-        let win;
-        
-        msg.edit(`끝말잇기가 시작되었습니다. ${p1}님이 단어를 입력해주세요`)
-        
-        while (endgame === 0) {
-        
-        if (thip === 0) {
-         const filter = (m) => m.author.id === p1;
-message.channel.awaitMessages(filter, {
-		max: 1
-    }).then((collected) => {
-let input = collected.first().content;
-eln = input.charAt(fullStr.length-1);
-msg.edit(`${p2}님! 단어를 입력해 주세요.`)
-thip = 1
-
-        })
-        
-        } else if (thip === 1) {
-        
-        
-        
-        } else if (thip === 2) {
-        
-        
+        class Game { 
+                constructor(){
+                        this.word = null;
+                        this.wordList = [];
+                }
+                start1(){
+                        message.channel.send('끝말잇기를 시작합니다. 1분안에 2명이 모이지 않으면 종료됩니다.').then((msg) => {
+                                msg.react('🎉')
+                               const filterr = (reaction, user) => reaction.emoji.name === '🎉' && user.id !== bot.user.id
+                                     msg.awaitReactions(filterr, {
+                                       maxUsers: 2
+                               }).then((collected) => {
+                                p1 = collected.array()[0].users.array()[1].id
+                                p2 = collected.array()[0].users.array()[2].id
+                                return this.start2(msg);
+                               });
+                        });
+                }
+                start2(){
+                        const filterr = (m) => m.author.id === p1			
+                        message.channel.send(`끝말잇기 시작! \n<@${p1}> ㄱㄱ`)
+                        message.channel.awaitMessages(filterr, {
+                                max: 1,
+                                time: 20000
+                    }).then((collected) => {
+                        if (!collected.first()) {return message.channel.send(`게임 끝! <@${p2}>의 승리!`)}
+                        let word = collected.first().content
+                        this.word = word;
+                        if (this.validationS(word)) {
+                                this.word = word;
+                                this.wordList.push(word);
+                                return this.user2(word);
+                        } else {                                 
+                                 return this.start2(word); }
+                        });
+                }
+                user1(word){
+                        const filterr = (m) => m.author.id === p1	
+                        if (!word) { word = this.word }					
+                        message.channel.send(`<@${p1}> ㄱㄱ ( ${word} => ? )`)
+                        message.channel.awaitMessages(filterr, {
+                                max: 1,
+                                time: 20000
+                    }).then((collected) => {
+                            if (!collected.first()) {return message.channel.send(`게임 끝! <@${p2}>의 승리!`)}
+                            let word = collected.first().content
+                        if (this.validation(word)) {
+                                        this.word = word;
+                                        this.wordList.push(word);
+                                        this.user2(word);
+                        } else { 
+                                return this.user1(word); }
+                        });
+                }
+                user2(word){
+                        const filterr = (m) => m.author.id === p2	
+                        if (!word) { word = this.word }			
+                        message.channel.send(` <@${p2}> ㄱㄱ (${word} => ?)`)
+                        message.channel.awaitMessages(filterr, {
+                                max: 1,
+                                time: 20000
+                    }).then((collected) => {
+                        if (!collected.first()) {return message.channel.send(`게임 끝! <@${p1}>의 승리!`)}
+                         let word = collected.first().content
+                        if (this.validation(word)) {
+                                        this.word = word;
+                                        this.wordList.push(word);
+                                        this.user1(word);
+                        } else { 
+                                return this.user2(); }
+                
+                        });
+                }
+                validation(word){
+                        if(!this.wordList.includes(word) && word.charAt(0) === this.word.slice(-1) &&  word.length>=2) {
+                                let answer = dict.filter(v => {
+                                        return v.charAt(0) === word.charAt(0).slice(-1) && v.length >= 2
+                                });
+                                if(!answer.includes(word)){	
+                                        message.channel.send('응~ 그딴거 없어~')		
+                                        return false;			
+                                } else { return true; }
+                        } else {
+                                if (this.wordList.includes(word)) {
+                                        message.channel.send('응 이미 했어~')
+                                        return false;
+                                }
+                                message.channel.send('응 그걸로 시작 안해~')
+                                        return false;
+                        }
+                }
+                validationS(word){
+                        if(word.length>=2) {
+                                let answer = dict.filter(v=>{
+                                        return v.charAt(0) === this.word.charAt(0).slice(-1) && v.length >= 2
+                                });
+                                if(!answer.includes(word)){	
+                                        message.channel.send('응~ 그딴거 없어~')		
+                                        return false;			
+                                } else { return true; }
+                        } else {
+                                message.channel.send('응 한글자 안되~')
+                                        return false;
+                        }
+                }
         }
         
-        }
-        
-        
-        });
-message.channel.send(`끝말잇기가 끝났습니다. ${win}님이 우승하셨습니다.`)
+        let game = new Game();
+        game.start1();
 
-});
-*/
-};
+
+}
+
 
 module.exports.help = {
-	name: "끝말잇기"
-};
+  name: "끝말잇기"
+}
